dc269f4713cccad097681437e346416462c16101
milenazahorska <milena.zahorska@stxnext.pl>
2017-12-04 22:50:14 +0100

    update zadania 2, zalazki struktury

--- zadania/model/patients.py
+++ zadania/model/patients.py
@@ -2,4 +2,16 @@
 
 class Patient:
 
-    def __init__(self, ):
+    def __init__(self, email, first_name, last_name):
+        self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def go_to_patients_page():
+        browser.find_element(By.LINK_TEXT, "My patients").click()
+        assert "My patients" in browser.find_element(By.TAG_NAME, "h1")
+
+# def get_patients_data():
+#     patients = []
+#     for patient in patients:
+#
--- zadania/tests/zadanie_2.py
+++ zadania/tests/zadanie_2.py
@@ -17,18 +17,36 @@ def log_in(username, password):
 
 def go_to_patients_page():
     browser.find_element(By.LINK_TEXT, "My patients").click()
-    # assert "My patients" in browser.find_element(By.CSS_SELECTOR, ".page-header")
+    assert "My patients" in browser.find_element(By.TAG_NAME, 'h1').text
 
 
 def get_emails():
     emails = []
     for email in browser.find_elements(By.XPATH, "//td[@class='email']/a"):
         emails.append(email.text)
-    print(emails)
+    return emails
+
+
+def get_first_name():
+    names = []
+    for name in browser.find_elements(By.XPATH, "//td[@class='fname']"):
+        names.append(name.text)
+    return names
+
+
+def get_last_name():
+    last_names = []
+    for last_name in browser.find_elements(By.XPATH, "//td[@class='lname']"):
+        last_names.append(last_name.text)
+    return last_names
 
 
 log_in("doctor_a@example.com", "admin123")
 assert "Doctor A" in browser.page_source
 go_to_patients_page()
-get_emails()
+email = get_emails()
+print(email)
 browser.close()
+
+
+
